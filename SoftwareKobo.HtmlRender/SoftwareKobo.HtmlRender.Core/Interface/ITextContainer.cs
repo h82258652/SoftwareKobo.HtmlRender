ea28ebb3434f58f0063cf593504528cefcc6321e
h82258652 <842053625@qq.com>
2014-11-30 17:40:40 +0800

    添加接口

---
+++ SoftwareKobo.HtmlRender/SoftwareKobo.HtmlRender.Core/Interface/ITextContainer.cs
@@ -0,0 +1,14 @@
+﻿using Windows.UI.Xaml;
+using Windows.UI.Xaml.Documents;
+
+namespace SoftwareKobo.HtmlRender.Core.Interface
+{
+    public interface ITextContainer
+    {
+        void Add(UIElement control);
+
+        void Add(Block block);
+
+        void Add(Inline inline);
+    }
+}
