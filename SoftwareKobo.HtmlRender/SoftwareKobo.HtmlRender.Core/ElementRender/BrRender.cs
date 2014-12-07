aaca0dde1836593b0205af3d48983deb5fe9eab8
h82258652 <842053625@qq.com>
2014-12-07 21:35:38 +0800

    fix fieldset next to br

--- SoftwareKobo.HtmlRender/SoftwareKobo.HtmlRender.Core/ElementRender/BrRender.cs
+++ SoftwareKobo.HtmlRender/SoftwareKobo.HtmlRender.Core/ElementRender/BrRender.cs
@@ -1,4 +1,5 @@
 ﻿using AngleSharp.DOM;
+using AngleSharp.DOM.Html;
 using SoftwareKobo.HtmlRender.Core.Interface;
 using Windows.UI.Xaml.Documents;
 
@@ -16,6 +17,14 @@ namespace SoftwareKobo.HtmlRender.Core.ElementRender
 
         public virtual void RenderElement(IElement element, ITextContainer parent, RenderContextBase context)
         {
+            if (element.PreviousElementSibling is IHtmlFieldSetElement)
+            {
+                return;
+            }
+            if (element.NextElementSibling is IHtmlFieldSetElement)
+            {
+                return;
+            }
             parent.Add(new LineBreak());
         }
     }
