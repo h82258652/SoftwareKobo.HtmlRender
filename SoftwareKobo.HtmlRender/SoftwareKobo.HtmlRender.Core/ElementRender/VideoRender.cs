3cd09cae7629dbd5745d5b8903d531264b1bbf01
h82258652 <842053625@qq.com>
2014-12-26 13:54:25 +0800

    修正video标签没有src的情况

--- SoftwareKobo.HtmlRender/SoftwareKobo.HtmlRender.Core/ElementRender/VideoRender.cs
+++ SoftwareKobo.HtmlRender/SoftwareKobo.HtmlRender.Core/ElementRender/VideoRender.cs
@@ -1,4 +1,5 @@
-﻿using AngleSharp.DOM;
+﻿using System.Linq;
+using AngleSharp.DOM;
 using AngleSharp.DOM.Html;
 using SoftwareKobo.HtmlRender.Core.Interface;
 using System;
@@ -20,11 +21,28 @@ namespace SoftwareKobo.HtmlRender.Core.ElementRender
         {
             var video = (IHtmlVideoElement)element;
 
+            string source = video.Source;
+
+            if (string.IsNullOrEmpty(source))
+            {
+                var sourceElement =
+                    video.Children.FirstOrDefault(
+                        temp => temp.TagName == "source" && temp.GetAttribute("type") == "video/mp4");
+                if (sourceElement != null)
+                {
+                    source = sourceElement.GetAttribute("src");
+                }
+            }
+
             var mediaElement = new MediaElement
             {
-                Source = new Uri(video.Source),
                 AreTransportControlsEnabled = true
             };
+
+            if (string.IsNullOrEmpty(source) == false)
+            {
+                mediaElement.Source = new Uri(source);
+            }
 
             parent.Add(mediaElement);
             context.RenderNode(element, parent);
